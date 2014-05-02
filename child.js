3e8f10ea60c6b5512bf5a3718bb7b0a6ca935d10
Bryan Haskin <bryan@ocgcreative.com>
2014-05-02 08:38:30 -0700

    fixed mobile scrolling

--- child.js
+++ child.js
@@ -1,11 +1,1 @@
 /*child.js*/
-
-( function( $ ) {
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
-    });
-} )( jQuery );
