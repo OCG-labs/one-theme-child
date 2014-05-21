e814dd326a66873534ba8d20dca429d760463b40
Bryan Haskin <bryan@ocgcreative.com>
2014-05-21 09:15:56 -0700

    added child module loader

--- functions.php
+++ functions.php
@@ -8,11 +8,18 @@
  * @author Matthew Hansen
  */
 
-if (file_exists(dirname( __FILE__ ) . '/lib/modules/loadModules.php')){
-  require_once dirname( __FILE__ ) . '/lib/modules/loadModules.php';
-}
+require_once dirname( __FILE__ ) . '/lib/modules/loadModules.php';
+
+require_once dirname( __FILE__ ) . '/lib/options/ot-options-settings.php';
+
+require_once dirname( __FILE__ ) . '/lib/core.php';
+
+global $theOneChild;
+$theOneChild = new TheOneChild;
 
 if(  !is_admin()  ) :
+  $theOneChild->assets();
+  //var_dump($theOneChild->getDirectory());
 
   if( !function_exists( 'otc_load_child_js' ) ) :
 
