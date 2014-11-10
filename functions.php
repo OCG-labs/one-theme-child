a67874b2db52b883dcd80214ba811f7c7980b392
Matthew Hansen <mattie02@gmail.com>
2014-11-10 12:53:57 -0800

    Moved child.js enqueue
    Moved child.js enqueue outside of the after_setup_theme.. this was causing the js not to load

--- functions.php
+++ functions.php
@@ -11,26 +11,13 @@
 
 function otChildFunctions() {
     require_once dirname( __FILE__ ) . '/lib/modules/loader.php'; // Legacy Loader
-
-
-  if(  !is_admin()  ) :
-
-
-    if( !function_exists( 'otc_load_child_js' ) ) :
-
-      function otc_load_child_js() {
-        wp_register_script( 'child_js', get_stylesheet_directory_uri().'/child.js', array( 'jquery' ), '1.0.0', true );
-        wp_enqueue_script( 'child_js' );
-
-      }
-
-      add_action( 'init', 'otc_load_child_js', 12 );
-
-    endif;
-
-  endif;
-
-
 }
 
 add_action( 'after_setup_theme', 'otChildFunctions', 10 );
+
+function otc_load_child_js() {
+    wp_register_script( 'child_js', get_stylesheet_directory_uri().'/child.js', array( 'jquery' ), '1.0.0', true );
+    wp_enqueue_script( 'child_js' );
+}
+
+add_action( 'init', 'otc_load_child_js', 12 );
